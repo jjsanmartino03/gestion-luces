955797bec4452868bb9bcf1fc35376ca603a159f
Julián Sanmartino <jjsanmartino03@gmail.com>
2023-10-23 22:01:25 -0300

    feat: setup frontend, add login view

---
+++ client/src/components/CustomButton.vue
@@ -0,0 +1,71 @@
+<template>
+  <button :class='computedClass' @click='handleClick'>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'CustomButton',
+  props: {
+    theme: {
+      type: String,
+      default: 'light' // possible values: 'light', 'dark'
+    },
+    variant: {
+      type: String,
+      default: 'primary' // possible values: 'primary', 'secondary'
+    }
+  },
+  computed: {
+    computedClass() {
+      return [this.theme, this.variant]
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* Light theme */
+button {
+  cursor: pointer;
+}
+
+.light.primary {
+  background-color: #007BFF;
+  color: white;
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 4px;
+}
+
+.light.secondary {
+  background-color: #6C757D;
+  color: white;
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 4px;
+}
+
+/* Dark theme */
+.dark.primary {
+  background-color: #001F3F;
+  color: white;
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 4px;
+}
+
+.dark.secondary {
+  background-color: #343A40;
+  color: white;
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 4px;
+}
+</style>
---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,75 @@
+<script setup>
+import Input from '../components/CustomInput.vue'
+import CustomButton from '../components/CustomButton.vue'
+</script>
+
+<template>
+  <main>
+    <div class='login-card'>
+      <form>
+        <h1>Ingresar</h1>
+        <Input input-id='username' label='Nombre de Usuario' v-model='username'
+               placeholder='Ingresa tu nombre de usuario' />
+        <Input placeholder='Ingresa tu contraseña' input-id='password' label='Contraseña' v-model='password'
+               type='password' />
+        <CustomButton theme='light' variant='primary' @click='login'>Iniciar sesión</CustomButton>
+      </form>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 500px;
+  height: 100%;
+  gap: 2rem;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.login-card {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+
+input {
+  outline: none;
+  border: none;
+  border-bottom: 1px solid gray;
+}
+
+</style>
+
+<script>
+import Input from '../components/CustomInput.vue'
+
+export default {
+  name: 'LoginView',
+  components: {
+    Input
+  },
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      alert('Hello!')
+    }
+  }
+}
+</script>
