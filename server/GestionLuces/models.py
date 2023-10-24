016839b9334fb2c080b13c63c52d90ea894a9ff4
Lucia Karlen <luuukarlen@gmail.com>
2023-10-23 21:35:55 -0300

    cambios db

--- server/GestionLuces/models.py
+++ server/GestionLuces/models.py
@@ -1,15 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Usuarios(models.Model):
-    ADMIN = 'admin'
-    USUARIO = 'usuario'
-    ROL_CHOICES = (
-        (ADMIN, 'Admin'),
-        (USUARIO, 'Usuario'),
-    )
-    nombre_usuario = models.CharField(max_length=30)
-    clave = models.CharField(max_length=250)
-    rol = models.CharField(max_length=10, choices=ROL_CHOICES, default=USUARIO)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
 
 class Aulas(models.Model):
     numero = models.IntegerField()
@@ -18,9 +11,10 @@ class TipoSensores(models.Model):
     nombre = models.CharField(max_length=30)
 
 class Sensores(models.Model):
-    ip_aula = models.ForeignKey(Aulas, on_delete=models.CASCADE)
-    last_signal_late = models.IntegerField()
+    aula = models.ForeignKey(Aulas, on_delete=models.CASCADE)
+    last_signal_late = models.DateTimeField()
     tipo = models.ForeignKey(TipoSensores, on_delete=models.CASCADE)
+    ip = models.CharField(max_length=30, unique=True)
 
 class Interacciones(models.Model):
     ENCENDIDO = 'encendido'
@@ -30,12 +24,12 @@ class Interacciones(models.Model):
         (APAGADO, 'apagado'),
     )
     tipo = models.CharField(max_length=10, choices=TIPO_CHOICES)
-    id_usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
-    id_sensor = models.ForeignKey(Sensores, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    sensor = models.ForeignKey(Sensores, on_delete=models.CASCADE)
     fecha = models.DateTimeField()
 
 class RegistrosLuces(models.Model):
-    id_sensor = models.ForeignKey(Sensores, on_delete=models.CASCADE)
+    sensor = models.ForeignKey(Sensores, on_delete=models.CASCADE)
     desde = models.DateTimeField()
     hasta = models.DateTimeField()
     estado = models.BooleanField()
