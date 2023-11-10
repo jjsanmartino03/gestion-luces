9e360ed80d168ec71210e97f5fcdf528357cda6b
Lucia Karlen <luuukarlen@gmail.com>
2023-11-09 21:06:17 -0300

    estadisticas generales y mejoras registros datos arduino

--- server/GestionLuces/views.py
+++ server/GestionLuces/views.py
@@ -4,11 +4,12 @@ from rest_framework import routers, serializers, viewsets
 from django.urls import path, include
 from GestionLuces.models import Aulas, Sensores, RegistrosLuces
 from rest_framework.response import Response
-import datetime
+from datetime import timedelta, datetime
 from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAdminUser
+from django.utils import timezone
 
 # Create your views here.
 
@@ -66,22 +67,62 @@ class RegistroDatosArduino (viewsets.ViewSet):
         estado = request.data.get('estado')
 
         last_signal = Aulas.objects.filter(ip = ip).first() #obtiene el aula con su ultima señal
-        last_signal.last_signal_date = datetime.datetime.now() #actualiza la ultima señal del aula
+        last_signal.last_signal_date = datetime.now() #actualiza la ultima señal del aula
         last_signal.save()
 
-        ultimo_registro = RegistrosLuces.objects.filter(sensor__aula__ip=ip, hasta=None).first() #obtiene el ultimo registro del sensor con la ip del sensor
+        ultimo_registro = RegistrosLuces.objects.filter(sensor__aula__ip=ip).last() #obtiene el ultimo registro del sensor con la ip del sensor
 
         if int(ultimo_registro.estado) != estado: #si el estado no es el mismo
             nuevo_registro = RegistrosLuces.objects.create( #se crea un nuevo campo con el nuevo estado
                 sensor = ultimo_registro.sensor,
-                desde = datetime.datetime.now(),
+                desde = datetime.now(),
                 estado = estado
             )
 
-            ultimo_registro.hasta = datetime.datetime.now() #se actualiza la fecha hasta del estado anterior
+            ultimo_registro.hasta = datetime.now() #se actualiza la fecha hasta del estado anterior
             ultimo_registro.save()
-        
+        else:
+            if ultimo_registro.desde.date() < datetime.now().date():
+                nuevo_registro = RegistrosLuces.objects.create(
+                    sensor = ultimo_registro.sensor,
+                    desde = datetime.now(),
+                    estado = estado
+                )
+
+                ultimo_registro.hasta = datetime(datetime.now().year, datetime.now().month, datetime.now().day - 1, 23, 59, 59)
+                ultimo_registro.save()
+
         return Response({})
+    
+class EstadisticasSemanales (viewsets.ViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+    def calcularConsumoDiario(self, fecha):
+        tiempo_total = timedelta()
+        registros = RegistrosLuces.objects.filter(
+            desde__date=fecha,
+            estado=1
+        )
+        for registro in registros:
+            hasta = registro.hasta
+            if not hasta: 
+                hasta = datetime.now()
+
+            tiempo_total += hasta - registro.desde
+        return tiempo_total
+
+
+    def list(self, request):
+        fecha_domingo = request.query_params.get('fecha_domingo')
+        fecha_domingo = datetime.strptime(fecha_domingo, '%Y-%m-%d').date()
+        lista = []
+        for i in range(7):
+            nueva_fecha = fecha_domingo + timedelta(days=i)
+            registro_diario = self.calcularConsumoDiario(nueva_fecha)
+            lista.append(round(registro_diario.total_seconds()/3600, 4))
+
+        return Response(lista)
 
 #Routers
 router = routers.DefaultRouter()
@@ -89,3 +130,4 @@ router.register(r'aulas', AulasViewSet)
 router.register(r'sensores', SensoresViewSet)
 router.register(r'registro_sensores', RegistroDatosArduino, basename='sensores')
 router.register(r'usuarios', UsuariosViewSet)
+router.register(r'estadistica_semanal', EstadisticasSemanales, basename='estadisticas')
