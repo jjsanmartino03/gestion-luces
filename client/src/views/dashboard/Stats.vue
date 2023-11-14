aead3026bddc960b5395c573fbdc94bcf082c5c3
ivandietta <diettaivan@gmail.com>
2023-11-13 21:52:20 -0300

    Agregar rama estadisticas_2

--- client/src/views/dashboard/Stats.vue
+++ client/src/views/dashboard/Stats.vue
@@ -32,7 +32,7 @@ import { RouterLink, RouterView } from 'vue-router'
       apexchart: VueApexCharts,
     },
     data() {
-      const data = [10.5, 9.7, 7.5, 6, 5, 8, 2];
+      const data = [8.5, 9.7, 7.5, 6, 5, 8, 2];
 
       // Calcula el promedio de los valores en data
       const average = data.reduce((acc, val) => acc + val, 0) / data.length;
@@ -42,10 +42,12 @@ import { RouterLink, RouterView } from 'vue-router'
       const minutes = Math.round((average % 1) * 60);
 
       return{
-        series: [{
-            name: 'Horas de uso al dia',
-            data: data,
-          }],
+        series: [
+          {
+            name: 'Horas de uso al día',
+            data: [10.5, 9.7, 7.5, 6, 5, 8, 2], // Actualiza los datos del gráfico con los promedios recibidos
+          },
+        ],
 
         chartOptions: {
           chart: {
@@ -76,9 +78,18 @@ import { RouterLink, RouterView } from 'vue-router'
           },
           },
           subtitle: {
-            text: 'Promedio semanal de uso de luces',
+            text:`Promedio semanal de uso de luces `,
+            style: {
+            fontSize: '13px',
+            },
           },
           xaxis: {
+            title: {
+            text: `${this.calcularFechasDeLaSemana().inicio} - ${this.calcularFechasDeLaSemana().fin}`,
+            style: {
+              fontSize: '13px',
+            },
+          },
             categories: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
           },
           yaxis: {
@@ -101,6 +112,54 @@ import { RouterLink, RouterView } from 'vue-router'
           }
         },
       } 
+    },
+    methods: {
+      calcularFechasDeLaSemana() {
+        const fechaActual = new Date();
+        const diaDeLaSemana = fechaActual.getDay();
+
+        const fechaDelDomingo = new Date(fechaActual);
+        fechaDelDomingo.setDate(fechaActual.getDate() - diaDeLaSemana);
+
+        const fechaDelSabado = new Date(fechaDelDomingo);
+        fechaDelSabado.setDate(fechaDelDomingo.getDate() + 6); // Sábado es 6 días después de domingo
+
+        const diaDomingo = fechaDelDomingo.getDate();
+        const mesDomingo = fechaDelDomingo.getMonth() + 1;
+        const añoDomingo = fechaDelDomingo.getFullYear();
+
+        const diaSabado = fechaDelSabado.getDate();
+        const mesSabado = fechaDelSabado.getMonth() + 1;
+        const añoSabado = fechaDelSabado.getFullYear();
+
+        const fechaInicio = `${diaDomingo < 10 ? '0' : ''}${diaDomingo}/${mesDomingo < 10 ? '0' : ''}${mesDomingo}/${añoDomingo}`;
+        const fechaFin = `${diaSabado < 10 ? '0' : ''}${diaSabado}/${mesSabado < 10 ? '0' : ''}${mesSabado}/${añoSabado}`;
+
+        return { inicio: fechaInicio, fin: fechaFin };
+      },
+      enviarFechaAlBackend() {
+        const fechaDelDomingo = this.calcularFechasDeLaSemana().inicio; // Obtén la fecha del domingo
+        // Realiza una solicitud GET al backend con la fecha
+        axios.get(`http://127.0.0.1:8000/api/estadistica_semanal/?fecha_domingo=${fechaDelDomingo}`)
+          .then(response => {
+            // Maneja la respuesta del backend que contiene la lista de promedios
+            const promedios = response.data; // Suponiendo que la respuesta es una lista de números
+            this.series = [
+              {
+                name: 'Horas de uso al día',
+                data: promedios, // Actualiza los datos del gráfico con los promedios recibidos
+              },
+            ];
+          })
+          .catch(error => {
+            // Maneja errores
+            console.error(error);
+          });
+      },
+    },
+    mounted() {
+      // Llama a la función para enviar la fecha al backend automáticamente al cargar la página
+      //this.enviarFechaAlBackend();
     },
   }
 </script>
